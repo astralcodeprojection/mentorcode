70e3dbf5b831c3792b6adef4795092fcd43eb7f4
bayareadesignco <moller76551@gotoltc.edu>
2022-01-31 20:55:50 -0600

    Initializing and working with sass / scss

--- userscript.php
+++ userscript.php
@@ -3,6 +3,10 @@
 session_start();
 //echo $_SESSION["userId"];
 //echo $_SESSION["logged_in"];
+<<<<<<< HEAD
+=======
+echo $_SESSION["userId"];
+>>>>>>> 4068b3e... Initializing and working with sass / scss
 if($_SESSION["logged_in"] != "true"){
     
 ?>
