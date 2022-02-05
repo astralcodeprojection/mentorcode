2ca47b6bdcda4988fb59e7ccefec25d83d0db2e8
bayareadesignco <moller76551@gotoltc.edu>
2022-02-05 03:40:16 -0600

    Fully converted to bootstrap 5 / still working on cart checkout deletion from table / further styling

--- navfooter.php
+++ navfooter.php
@@ -1,23 +1,25 @@
 <br><br><br>
-  <nav class="navbar navbar-expand-lg navbar-light bg-primary">
-    <div class="container-fluid">
-        
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                <li class="nav-item">
-                    <a class="nav-link disabled" aria-current="page" href="#">Teds Tasty Taco Truck</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link active" href="admin.php">Employee Portal</a>
-                </li>
-              
-
-            </ul>
+<div class="footer">
+    <nav class="navbar navbar-expand-lg navbar-light bg-primary">
+        <div class="container-fluid">
             
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+                aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link disabled" aria-current="page" href="#">Teds Tasty Taco Truck</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active" href="admin.php">Employee Portal</a>
+                    </li>
+                
+
+                </ul>
+                
+            </div>
         </div>
-    </div>
-</nav>
+    </nav>
+</div>
