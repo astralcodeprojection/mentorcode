c2c6826080cd89bdf5394d3b26493b9ca02dad3b
bayareadesignco <moller76551@gotoltc.edu>
2022-03-28 06:28:59 -0500

    Working with dynamic category generation / generating guest user sessions

--- menu.php
+++ menu.php
@@ -1,3 +1,6 @@
+<?php
+session_start();
+?>
 <!DOCTYPE html>
 <html>
 
@@ -21,44 +24,155 @@
         <br><br>
 
   <!-- START THE DESCRIPTION CONTENT  -->
-      
+   
       <div class="row">
-          <div class="col-md-3">
+        <div class="col-md-12">
+            <article class="left">
             
-        </div>
-        <div class="col-md-6">
-            <article>
+            <?php
+
+                require_once("connect-db.php");
+                
+                $sql0 = "SELECT * FROM menuitems";
+                $error0 = "";
             
+                $statement0 = $db->prepare($sql);
+                
             
-            <br>
-            <img class="qs" src="img/cq.jpg">
-            <h3>Cheese Quesadilla - 7$</h3>
-            <p>A blend of traditional cheeses melted in a flour totilla.</p>
-            <br>
-            <img class="cq" src="img/9522c8dc-24c7-4ba6-b3e8-81cc3ff22f4c.jpg">
-            <h3>Chicken Quesadilla - 7$</h3>
-            <p>A blend of traditional cheeses with grilled chicken melted in a flour totilla.</p>
-            <br>
-            <img class="pca" src="img/132477_640x428.jpg">
-            <h3>Pollo con Arroz - 9$</h3>
-            <p>Rice topped with cheese coupled with seasoned grilled chicken</p>
-            <br>
-            <img class="ec" src="img/el.jpg">
-            <h3>Enchiladas - 7$</h3>
-            <p>A cheese topped beef enchilada that is paired with a traditional spicy red sauce.</p><br>
-            <br>
-            <img class="taco" src="img/beef-tacos-I-howsweeteats.com-11.jpg">
-            <h3>Beef Tacos - 6$</h3>
-            <p>A pair of beef tacos topped with lettuce and cheese.</p>
-            <h3>Steak Tacos - 8$</h3>
-            <p>A pair of steak tacos topped with lettuce and cheese.</p><br>
+                if($statement0->execute()){
+                    $categories = $statement0->fetchAll();
+                    $statement0->closeCursor();
+                    
+                }else{
+                    $error0= "Error finding items.";
+                }
             
+               
+            ?>
+            <div class="cardComponent">
+           
+                <div class="dropdown">
+                    <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        Catagories
+                    </button>
+                    <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                        <?php
+                        foreach($catagories as $c){?>
+                    
+                            <a class="dropdown-item" href="#"><?php echo$c["catagory"];?></a>
+                         <?php } ?>
+                    </div>
+                </div>
+                
+            </div>
+            <br><br><br>
+            <?php
+
+                require_once("connect-db.php");
+                if (isset($_GET["page"])) { $page  = $_GET["page"]; } 
+                else { $page=1; };
+                 $start_from = ($page-1) * $results_per_page;
+                $sql = "SELECT * FROM menuitems ORDER BY itemId ASC LIMIT $start_from, $results_per_page";
+                $error1 = "";
             
+                $statement1 = $db->prepare($sql);
+                
+            
+                if($statement1->execute()){
+                    $customers = $statement1->fetchAll();
+                    $statement1->closeCursor();
+                    
+                }else{
+                    $error1= "Error finding items.";
+                }
+            
+               
+            ?>
+            
+            <div class="cardComponent">
+           <?php
+                foreach($customers as $c){?>
+                  <!-- foreach($customers as $c->fetch_assoc()){?>  -->
+                    
+                            <div class="card" style="width: 18rem;">
+                                <img src="<?php echo $c["img"];?>" class="card-img-top" alt="...">
+                                <div class="card-body">
+                                    <h5 class="card-title"><?php echo $c["name"];?></h5>
+                                    <h5 class="card-title"> <?php echo $c["price"]." $";?></h5>
+                                    <p class="card-text"><?php echo $c["descr"];?></p>
+                                    <form action="addcart.php" method="post">
+                                        <input type="hidden" name="itemId" value="<?php echo $c["itemId"];?>">
+                                        <button class="btn btn-primary" type="submit">Add to Cart</button>
+                                    </form>
+                                </div>
+                            </div>
+                    
+                    
+            <?php } ?>
+            </div>
+            <br><br><br>
+            <div class="row">
+                <div class="col-md-4">
+                  
+                </div>
+                <div class="col-md-4">
+                  <?php
+                        require_once("connect-db.php");
+                        $sql3 = "SELECT COUNT(itemId) AS total FROM menuitems";  
+                                
+                        $error3 = "";
+
+                        
+                        $statement3 = $db->prepare($sql3);
+                        
+                    
+                    
+                        if($statement3->execute()){
+                            $pagenation = $statement3->fetchAll();
+                            
+                            $statement3->closeCursor();
+                            $total_pages = ceil($pagenation[0]["total"] / $results_per_page);
+                        
+                       // echo "<a href='olo.php?page=".($page-1)."' class='button text-secondary fw-bold'>Previous  </a>"; 
+                        ?>
+                        <?php
+                        if($page <= 1) {
+                        ?>
+                        <a class="page-link disabled">PreviousTest </a>
+                        <?php } else{ ?>
+                            <a class="page-link" href="olo.php?page=<?= ($page-1);?>">Previous </a>
+                        <?php } ?>
+                        <?php
+                        for ($i=1; $i<=$total_pages; $i++) {  // print links for all pages
+                            echo "<a href='olo.php?page=".$i."' class=text-secondary ";
+                            if ($i==$page)  echo " class='curPage text-secondary '";
+                            echo ">".$i."</a> ";
+                        }    
+                        //echo "<a href='olo.php?page=".($page+1)."' class='button text-secondary fw-bold'> Next</a>";
+                        }else{
+                            $error3= "Error Loading.";
+                        }
+                        ?>
+                        <?php
+                        if($page == $total_pages) {
+                        ?>
+                        <a class="page-link disabled">NextTest </a>
+                        <?php } else{ ?>
+                            <a class="page-link" href="olo.php?page=<?= ($page+1);?>">Next </a>
+                        <?php } ?>
+                   
+                </div>
+                <div class="col-md-4">
+                  
+                </div>
+            </div>
+           
         </article>
         </div>
-        <div class="col-md-3">
-            
-        </div>
+
+	
+        
+       
       </div>
   </div>
       <?php include("navfooter.php");?>
